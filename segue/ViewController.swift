bf283d8679779e3ad8188e45b593cca791b8cdcd
Kate <kate@gmail.com>
2024-07-15 19:46:30 +0800

    Initial Commit

---
+++ segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segue
+//
+//  Created by kate on 2024/7/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
